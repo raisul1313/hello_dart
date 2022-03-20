a7f522e6b983d5e9538b276f685ae5846db85fad
raisul1313 <raisul.islam1313@gmail.com>
2022-03-20 21:53:43 +0600

    first commit

---
+++ bin/hello_dart_verse.dart
@@ -0,0 +1,146 @@
+void main(List<String> arguments) {
+  int x = 5;
+  x &= 3;
+  print(x);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // //entry point for execution
+  //
+  // int number = 34;
+  // int numberTwo = 2;
+  // double pi = 3.14;
+  // double gravity = 9.8;
+  //
+  // //switch
+  //
+  // var age = 20;
+  //
+  // switch (age){
+  //   case 19:
+  //     print("Ond enough");
+  //     break;
+  //   case 47:
+  //     print("Still good");
+  //     break;
+  //   case 54:
+  //     print("Ok");
+  //   break;
+  //   default:
+  //     print ("not ok");
+  // }
+
+
+
+  // do{
+  //   print('Hello');
+  // }while(number <  34);
+
+  //Variable = spot in memory, or just a bucket to hold a value.
+
+  // var country;  //string
+  // String name;
+  // name = "Raisul";
+  // country = "Japan";
+  //
+  // int age = 13;
+  //
+  // double number = 23.45;
+  //
+  // bool isTrue = true;
+  // bool isFalse = false;
+  //
+  // const pi = 3.14;
+  // final pie = 3.14;
+
+  // String fastName = "james";
+  // String lastName = "Bond";
+  // int age = 45;
+  //
+  // print("$fastName $lastName is $age");
+  // print("Hello There $fastName ${lastName.toUpperCase()}");
+
+
+  // //for loops
+  // for(var i = 0; i < 10 ; i++){
+  //   if( i % 3 == 0){
+  //     print("hello $i");
+  //   }
+  // }
+  //
+  // var fruit = "Mango";
+  // for(var i = 0; i < 10 ; i++){
+  //   if( i % 3 == 0){
+  //     print("$fruit $i");
+  //   }
+  // }
+
+  // for( int i = 0; i < 10; i++){
+  //   print("$i");
+  // }
+
+  // while and do while loop
+
+  // while(true){  //infinite loop
+  //   print("Going....");
+  // }
+
+  // if ( !(number != 100) || number >= 100) {  //false or true
+  //   print("not a hundred");
+  // } else {
+  //   print("yes");
+  // }
+  // if ( !(number != 100) && number <= 100) {   //False AND True
+  //   print("not a hundred");
+  // } else {
+  //   print("yes");
+  // }
+  // // If Statement
+  // if (a != 34) {
+  //   print("if True, This will Run");
+  // } else {
+  //   print("Else Running");
+  // }
+
+  // print (a is! bool);
+  // print (4 >= 2 ); //greater than or equal
+  // print (4 <= 2 ); //less than or equal
+  // print (4 < 2 ); //less
+  // print (4 == 2 ); //equal
+  // (2 != 2 );  //not equal
+  // print (4 > 2 ); //grater
+  // print (2 > 2 );  //grater
+  // print(pi + gravity);
+  // print(pi - gravity);
+  // print(pi * gravity);
+  // print(pi / gravity);
+  // print(a + b);
+  // print(a - b);
+  // print(a * b);
+  // print(b / a);
+  // print(b % a);
+  // print(isFalse);
+  // print(age);
+  // print(number);
+  // print(name);
+}
---
+++ test/hello_dart_verse_test.dart
@@ -0,0 +1,8 @@
+import 'package:hello_dart_verse/hello_dart_verse.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
